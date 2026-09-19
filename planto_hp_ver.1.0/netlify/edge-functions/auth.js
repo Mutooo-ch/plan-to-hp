6fd40c172cad6f2b3f4bcff7dcf81bf280642d75
sket1374@gmail.com <159230395+Mutooo-ch@users.noreply.github.com>
2026-09-19 20:05:14 +0900

    Update auth.js

--- planto_hp_ver.1.0/netlify/edge-functions/auth.js
+++ planto_hp_ver.1.0/netlify/edge-functions/auth.js
@@ -1,1 +1,18 @@
+export default async (request, context) => {
+  // ユーザー名: planto 、 パスワード: planto0321
+  // この2つを暗号化した文字列が "cGxhbnRvOnBsYW50bzAzMjE=" になります
+  const expected = "Basic cGxhbnRvOnBsYW50bzAzMjE=";
+  const auth = request.headers.get("authorization");
 
+  if (auth === expected) {
+    return context.next(); // パスワードが合っていればサイトを表示
+  }
+
+  // 合っていない、または未入力の場合は入力画面を出す
+  return new Response("Unauthorized", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Team Only"',
+    },
+  });
+};
